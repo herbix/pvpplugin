9a7a344d0fa9e383539ae7f79616cde1aa525418
herbix <herbix@163.com>
2014-06-19 14:59:22 +0800

    功能大体完成

--- src/main/java/com/github/herbix/pvpplugin/PvpPlugin.java
+++ src/main/java/com/github/herbix/pvpplugin/PvpPlugin.java
@@ -1,10 +1,17 @@
 package com.github.herbix.pvpplugin;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.UUID;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -16,6 +23,10 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerLoginEvent.Result;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
@@ -28,11 +39,48 @@ public class PvpPlugin extends JavaPlugin implements Listener, Runnable {
 	private Map<UUID, State> playerStates = new HashMap<UUID, State>();
 	private Scoreboard sb;
 	private boolean gameStart = false;
+	private Location redStart;
+	private Location blueStart;
+	private List<Recipe> pvpAddRecipes = new ArrayList<Recipe>();
 
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
+		initScoreboard();
+		initRecipes();
+		getServer().getScheduler().scheduleSyncRepeatingTask(this, this, 0, 20);
+	}
 
+	private void initRecipes() {
+		Recipe recipe = new ShapedRecipe(new ItemStack(Material.GHAST_TEAR))
+				.shape(" # ", "$$$")
+				.setIngredient('#', Material.GOLD_INGOT)
+				.setIngredient('$', Material.WATER_BUCKET);
+		getServer().addRecipe(recipe);
+		pvpAddRecipes.add(recipe);
+		
+		recipe = new ShapedRecipe(new ItemStack(Material.BLAZE_ROD))
+				.shape("%%%", "%#%", "$$$")
+				.setIngredient('#', Material.TORCH)
+				.setIngredient('$', Material.GOLD_NUGGET)
+				.setIngredient('%', Material.ROTTEN_FLESH);
+		getServer().addRecipe(recipe);
+		pvpAddRecipes.add(recipe);
+		
+		recipe = new ShapelessRecipe(new ItemStack(Material.GLOWSTONE_DUST))
+				.addIngredient(1, Material.SUGAR)
+				.addIngredient(1, Material.BLAZE_POWDER);
+		getServer().addRecipe(recipe);
+		pvpAddRecipes.add(recipe);
+		
+		recipe = new ShapelessRecipe(new ItemStack(Material.NETHER_WARTS))
+				.addIngredient(1, Material.SEEDS)
+				.addIngredient(1, Material.REDSTONE);
+		getServer().addRecipe(recipe);
+		pvpAddRecipes.add(recipe);
+	}
+
+	private void initScoreboard() {
 		ScoreboardManager sm = getServer().getScoreboardManager();
 		Scoreboard mb = sm.getMainScoreboard();
 		sb = sm.getNewScoreboard();
@@ -66,18 +114,15 @@ public class PvpPlugin extends JavaPlugin implements Listener, Runnable {
 			states = sb.registerNewObjective("States", "dummy");
 		}
 
-		states.setDisplayName("¡ìc¡ìlHealth");
+		states.setDisplayName("¡ìa¡ìlHealth");
 		states.setDisplaySlot(DisplaySlot.SIDEBAR);
-
-		getServer().getScheduler().scheduleSyncRepeatingTask(this, this, 0, 20);
 	}
 
 	@Override
 	public void onDisable() {
 		HandlerList.unregisterAll((JavaPlugin) this);
 
-		ScoreboardManager sm = getServer().getScoreboardManager();
-		Scoreboard mb = sm.getMainScoreboard();
+		Scoreboard mb = getServer().getScoreboardManager().getMainScoreboard();
 
 		mb.getTeam("pvp-red").unregister();
 		mb.getTeam("pvp-blue").unregister();
@@ -86,17 +131,115 @@ public class PvpPlugin extends JavaPlugin implements Listener, Runnable {
 		Player[] players = getServer().getOnlinePlayers();
 		for(Player player : players) {
 			player.setScoreboard(mb);
+			player.setCompassTarget(player.getWorld().getSpawnLocation());
+		}
+
+		Iterator<Recipe> it = getServer().recipeIterator();
+		while(it.hasNext()) {
+			if(pvpAddRecipes.contains(it.next())) {
+				it.remove();
+			}
 		}
 		
 		getServer().getScheduler().cancelTasks(this);
 	}
 	
 	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(sender instanceof Player) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("red") || cmd.getName().equalsIgnoreCase("blue")) {
+			if(sender instanceof Player) {
+				if(gameStart) {
+					sender.sendMessage("Cannot change team during the game.");
+					return true;
+				}
+				
+				boolean isRed = cmd.getName().equalsIgnoreCase("red");
+				Player player = (Player) sender;
+				Scoreboard mb = getServer().getScoreboardManager().getMainScoreboard();
+				
+				mb.getTeam("pvp-" + cmd.getName().toLowerCase()).addPlayer(player);
+				
+				playerStates.get(player.getUniqueId()).team = isRed ? 1 : 2;
+
+				for(Entry<UUID, State> entry : playerStates.entrySet()) {
+					UUID playerId = entry.getKey();
+					Player player2 = getServer().getPlayer(playerId);
+					
+					if(player2 != null) {
+						player2.sendMessage("¡ìe" + player.getName() + "¡ìr is changed to team " + (isRed ? "¡ìcred" : "¡ì9blue") + ".");
+					}
+				}
+				
+				return true;
+			} else {
+				sender.sendMessage("This command can only be run by a player.");
+				return true;
+			}
+		} else if(cmd.getName().equalsIgnoreCase("pvpstart")) {
+			if(gameStart) {
+				sender.sendMessage("Game has started.");
+				return true;
+			}
 			
+			for(Entry<UUID, State> entry : playerStates.entrySet()) {
+				State state = entry.getValue();
+				if(state.team == 0) {
+					sender.sendMessage("There's one who didn't choose team.");
+					return true;
+				}
+			}
+			
+			World world = Bukkit.getWorld("world");
+
+			redStart = findHighestLocation(world, 496, 496, 16, 16);
+			blueStart = findHighestLocation(world, -512, -512, 16, 16);
+
+			for(Entry<UUID, State> entry : playerStates.entrySet()) {
+				Player player = getServer().getPlayer(entry.getKey());
+				State state = entry.getValue();
+				if(state.team == 1) {
+					player.teleport(redStart);
+					player.setBedSpawnLocation(redStart, true);
+				}
+				if(state.team == 2) {
+					player.teleport(blueStart);
+					player.setBedSpawnLocation(blueStart, true);
+				}
+			}
+			
+			world.setTime(0);
+			
+			gameStart = true;
+			
+			sender.sendMessage("Game start. Have fun!");
+			return true;
+			
+		} else if(cmd.getName().equalsIgnoreCase("pvpstop")) {
+			if(!gameStart) {
+				sender.sendMessage("Game has not started.");
+				return true;
+			}
+
+			gameStart = false;
+			sender.sendMessage("Game stop.");
+			return true;
 		}
 		return false;
+	}
+
+	private Location findHighestLocation(World world, int sx, int sz, int w, int h) {
+		int hx = sx, hy = -1, hz = sz;
+		for(int i=sx; i<sx+w; i++) {
+			for(int j=sz; j<sz+h; j++) {
+				int hm = world.getHighestBlockYAt(i, j);
+				if(hm > hy) {
+					hy = hm;
+					hx = i;
+					hz = j;
+				}
+			}
+		}
+		return new Location(world, hx, hy, hz);
 	}
 
 	@EventHandler
@@ -119,7 +262,6 @@ public class PvpPlugin extends JavaPlugin implements Listener, Runnable {
 			}
 		} else {
 			State state = playerStates.get(event.getPlayer().getUniqueId());
-			state.oldOnline = state.online;
 			state.online = true;
 		}
 		event.getPlayer().setScoreboard(sb);
@@ -129,25 +271,32 @@ public class PvpPlugin extends JavaPlugin implements Listener, Runnable {
 	public void onDeath(PlayerDeathEvent event) {
 		Player dead = event.getEntity();
 		State s = playerStates.get(dead.getUniqueId());
-		s.oldDeathCount = s.deathCount;
-		s.deathCount++;
+		s.death(dead.getKiller() != null);
 
 		Player killer = dead.getKiller();
 		if(killer != null) {
 			s = playerStates.get(killer.getUniqueId());
-			s.oldKillCount = s.killCount;
-			s.killCount++;
+			s.kill();
+		}
+		
+		if(gameStart) {
+			State state = playerStates.get(dead.getUniqueId());
+			if(state.team == 1) {
+				dead.setBedSpawnLocation(redStart, true);
+			}
+			if(state.team == 2) {
+				dead.setBedSpawnLocation(blueStart, true);
+			}
 		}
 	}
-	
+
 	@EventHandler
 	public void onQuit(PlayerQuitEvent event) {
 		if(!gameStart) {
 			State state = playerStates.remove(event.getPlayer().getUniqueId());
-			sb.resetScores(state.getTeamColor() + state.playerName + "(" + state.killCount + "/" + state.deathCount + ")");
+			sb.resetScores(state.getOldScoreString());
 		} else {
 			State state = playerStates.get(event.getPlayer().getUniqueId());
-			state.oldOnline = state.online;
 			state.online = false;
 		}
 	}
@@ -168,14 +317,56 @@ public class PvpPlugin extends JavaPlugin implements Listener, Runnable {
 				if(!gameStart && !player.isDead()) {
 					player.setHealth(20);
 				}
+
+				checkPlayerLocation(player);
+				setPlayerCompass(player, state);
+				
 				health = (int)player.getHealth();
 			}
 
-			sb.resetScores(state.getOldTeamColor() + state.playerName + "(" + state.oldKillCount + "/" + state.oldDeathCount + ")");
-			states.getScore(state.getTeamColor() + state.playerName + "(" + state.killCount + "/" + state.deathCount + ")").setScore(health);
+			sb.resetScores(state.getOldScoreString());
+			states.getScore(state.newScoreString()).setScore(health);
 		}
 
 		states.setDisplaySlot(DisplaySlot.SIDEBAR);
+	}
+
+	private void setPlayerCompass(Player player, State state) {
+		Player[] players = getServer().getOnlinePlayers();
+		Location locPlayer = player.getLocation();
+		Location locTarget = null;
+		double distance = Double.MAX_VALUE;
+		
+		for(Player target : players) {
+			State s = playerStates.get(target.getUniqueId());
+			if(s != null) {
+				if(s.team != state.team) {
+					Location l = target.getLocation();
+					double d = l.distance(locPlayer);
+					if(d < distance) {
+						distance = d;
+						locTarget = l;
+					}
+				}
+			}
+		}
+		
+		if(locTarget != null) {
+			player.setCompassTarget(locTarget);
+		} else {
+			player.setCompassTarget(player.getWorld().getSpawnLocation());
+		}
+	}
+
+	private void checkPlayerLocation(Player player) {
+		Location loc = player.getLocation();
+		if(loc.getX() < -512) loc.setX(-512);
+		if(loc.getZ() < -512) loc.setZ(-512);
+		if(loc.getX() > 512) loc.setX(512);
+		if(loc.getZ() > 512) loc.setZ(512);
+		if(!loc.equals(player.getLocation())) {
+			player.teleport(loc);
+		}
 	}
 
 	public void i(String msg) {
--- src/main/java/com/github/herbix/pvpplugin/State.java
+++ src/main/java/com/github/herbix/pvpplugin/State.java
@@ -5,26 +5,42 @@ public class State {
 	public String playerName;
 
 	public int team;
-	public int killCount;
-	public int deathCount;
 	public boolean online = true;
-	
-	public int oldTeam;
-	public int oldKillCount;
-	public int oldDeathCount;
-	public boolean oldOnline = true;
+
+	private int killCount;
+	private int deathCount;
+	private int continuousKillCount = 0;
+
+	private String oldScoreString = "";
 	
 	public State(String player) {
 		this.playerName = player;
 	}
 	
-	private static final String[] color = {"", "¡ìc", "¡ì9", "¡ì8"};
-	
-	public String getOldTeamColor() {
-		return oldOnline ? color[oldTeam] : color[3];
-	}
+	private static final String[] color = {"", "¡ìc", "¡ì9", "¡ì4", "¡ì1"};
 	
 	public String getTeamColor() {
-		return online ? color[team] : color[3];
+		return online ? color[team] : color[team+2];
+	}
+
+	public String getOldScoreString() {
+		return oldScoreString;
+	}
+	
+	public String newScoreString() {
+		oldScoreString = getTeamColor() + playerName + "¡ìr(" + killCount + "/" + deathCount + ")";
+		return oldScoreString;
+	}
+	
+	public void kill() {
+		killCount++;
+		continuousKillCount++;
+	}
+	
+	public void death(boolean byPlayer) {
+		deathCount++;
+		if(byPlayer) {
+			continuousKillCount = 0;
+		}
 	}
 }
